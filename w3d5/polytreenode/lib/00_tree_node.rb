2e26cdc54600582cb41603ec98cb8a1020c6d03e
colewendling <colewendling@utexas.edu>
2020-12-04 17:19:28 -0600

    added parent= & add_child

--- w3d5/polytreenode/lib/00_tree_node.rb
+++ w3d5/polytreenode/lib/00_tree_node.rb
@@ -1,13 +1,68 @@
 class PolyTreeNode
-    attr_reader :value, :children, :parent
-    def initialize(value,children=[])       
+
+    attr_accessor :value
+    attr_reader :parent
+
+    def initialize(value = nil)       
         @value = value 
-        @children = children
         @parent = nil
+        @children = []
     end    
 
-    def parent= 
-        
+    def children
+        @children.dup
+    end
+
+    def parent=(parent) 
+        #@parent = @children
+        #@children[1] = 
+        return if self.parent == parent # not running script if new parent is old parent
+
+
+        #self is the PolytreeNode instance
+
+        # self = #<PolyTreeNode:0x00007fdf3a026478 
+        # @value="a", @children=["b", "c"], @parent=nil>
+
+        #           "a"
+        #        "b"   "c"
+
+        # node.parent = "new parent"
+
+        # 1. if node has parent, remove previous 
+        #     parent
+
+        if self.parent # continues if node has a parent
+            self.parent._children.delete(self)
+        end
+
+         @parent = parent
+        self.parent._children << self unless self.parent.nil?
+
+         self #modified self with new parent
+    end
+
+
+    # b = #<PolyTreeNode:0x00007fdf3a026478 
+    # @value="b", @children=["d", "e"], @parent="a">
+
+
+    # w = #<PolyTreeNode:0x00007fdf3a026478 
+    # @value="w", @children=["x", "y"], @parent="b">
+
+
+
+    # b.add_child(w)
+
+    # my_node.add_child("d")  <---- new child
+
+    def add_child(child)
+        #return if child.parent == self
+        child.parent = self
+    end
+
+    def _children
+       @children
     end
 
 end
