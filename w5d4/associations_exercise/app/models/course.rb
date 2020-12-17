e945ef01b6403c97e8908306de159d9589e45c8f
chris <chrispikacho@gmail.com>
2020-12-17 15:26:59 -0800

    Added associations to all

--- w5d4/associations_exercise/app/models/course.rb
+++ w5d4/associations_exercise/app/models/course.rb
@@ -1,2 +1,30 @@
+#   create_table "courses", force: :cascade do |t|
+#     t.string "name"
+#     t.integer "prereq_id"
+#     t.integer "instructor_id"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#   end
+
 class Course < ApplicationRecord
+
+    has_many :enrollments,
+        class_name: 'Enrollment',
+        foreign_key: :course_id,
+        primary_key: :id
+
+    has_many :students,
+        through: :enrollments,
+        source: :user
+
+    belongs_to :instructor,
+        class_name: 'User',
+        foreign_key: :instructor_id,
+        primary_key: :id
+      
+    belongs_to :prerequisites,
+        class_name: 'Course',
+        foreign_key: :prereq_id,
+        primary_key: :id
+
 end
--- w5d4/associations_exercise/app/models/user.rb
+++ w5d4/associations_exercise/app/models/user.rb
@@ -1,2 +1,20 @@
+#   create_table "users", force: :cascade do |t|
+#     t.string "name"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#   end
+
 class User < ApplicationRecord
+
+    has_many :enrollments,
+        class_name: 'Enrollment',
+        foreign_key: :student_id,
+        primary_key: :id
+
+    has_many :courses,
+        through: :enrollments,
+        source: :course
+        # class_name: 'Course'
+
+
 end
