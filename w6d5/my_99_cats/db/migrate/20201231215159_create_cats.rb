7dee468c54258e2b6a5a0fb49e45ba3fdfd839b1
colewendling <colewendling@utexas.edu>
2020-12-31 16:02:18 -0600

    migrate

--- w6d5/my_99_cats/db/migrate/20201231215159_create_cats.rb
+++ w6d5/my_99_cats/db/migrate/20201231215159_create_cats.rb
@@ -4,7 +4,7 @@ class CreateCats < ActiveRecord::Migration[5.2]
       t.date :birth_date, null: false
       t.string :color, null: false
       t.string :name, null: false
-      t.string :sex, null: false
+      t.string :sex, limit: 1, null: false
       t.text :description
 
       t.timestamps
