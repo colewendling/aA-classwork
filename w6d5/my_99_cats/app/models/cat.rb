0dc77eb8ff327fe5801bacbe7f1ba1f063480bc4
Bogey <jeffreysbogart@gmail.com>
2020-12-31 14:55:16 -0800

    Adds age method to cats model

--- w6d5/my_99_cats/app/models/cat.rb
+++ w6d5/my_99_cats/app/models/cat.rb
@@ -1,2 +1,15 @@
+require 'action_view'
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+    COLORS = %w(brown black white orange)
+    
+    validates :color, inclusion: COLORS
+    validates :sex, inclusion: %w(M F)
+    validates :birth_date, presence: true
+
+    def age
+        current_year = Time.now.year
+        current_year - self.birth_date.year
+    end
+
 end
