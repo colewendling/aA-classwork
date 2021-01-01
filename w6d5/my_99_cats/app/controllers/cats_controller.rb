274e5c72d03e4092f135f32283162598a5ced01f
Bogey <jeffreysbogart@gmail.com>
2020-12-31 16:38:08 -0800

    Adds show method

--- w6d5/my_99_cats/app/controllers/cats_controller.rb
+++ w6d5/my_99_cats/app/controllers/cats_controller.rb
@@ -15,4 +15,23 @@ class CatsController < ApplicationController
         end
     end
 
+    def new
+        @cat = Cat.new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
+    end
+
+    private
+    def cat_params
+
+    end
+
 end
