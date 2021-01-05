515c54eaebcb54ac402e945f69d9a1abb0cc9fc2
colewendling <colewendling@utexas.edu>
2021-01-04 18:27:00 -0600

    user model session token work

--- w7d1/99_cats_auth/app/models/user.rb
+++ w7d1/99_cats_auth/app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+    after_initialize do |user|
+        user.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
 end
+
+
+
