d9aba73defc629f7e45f3bb73c38d8d9d282f651
AlexeyS <xengawe@gmail.com>
2021-01-10 22:43:31 -0800

    complete bands controller

--- w7d2/music_app/config/routes.rb
+++ w7d2/music_app/config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   
-  get 'users/create'
-  get 'users/new'
-  get 'users/index'
-  get 'users/show'
- resource :session, only: [:new, :create, :destroy]
- resources :users
+  resources :bands 
+  resource :session, only: [:new, :create, :destroy]
+  resources :users
 end
