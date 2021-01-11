210bea031206123b91e9f683be9f1ae9051d2cfa
colewendling <colewendling@utexas.edu>
2021-01-11 00:12:05 -0600

    add users controller

--- w7d2/music_app/app/controllers/application_controller.rb
+++ w7d2/music_app/app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
-  def require_logged_id!
+  def require_logged_in!
     redirect_to new_session_url unless logged_in? 
   end
 
--- w7d2/music_app/config/routes.rb
+++ w7d2/music_app/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'users/create'
+  get 'users/new'
+  get 'users/index'
+  get 'users/show'
  resource :session, only: [:new, :create, :destroy]
  resources :users
 end
