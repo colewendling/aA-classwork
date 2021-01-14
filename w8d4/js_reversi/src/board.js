646f552582d0cef50abc06521f2cac09685c8b3f
colewendling <colewendling@utexas.edu>
2021-01-14 17:42:13 -0600

    fix line 164 j++

--- w8d4/js_reversi/src/board.js
+++ w8d4/js_reversi/src/board.js
@@ -161,7 +161,7 @@ Board.prototype.validMoves = function (color) {
   const validPositions = [];
 
   for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; i++) {
+    for (let j = 0; j < 8; j++) {
       if (this.validMove([i, j], color)) {
         validPositions.push([i,j]);
       }
