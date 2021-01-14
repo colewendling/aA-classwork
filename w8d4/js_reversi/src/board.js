5cf3efc85c34646a4c7e9cb51094f5dde0e20bba
colewendling <colewendling@utexas.edu>
2021-01-14 16:33:04 -0600

    _postionsToFlip method

--- w8d4/js_reversi/src/board.js
+++ w8d4/js_reversi/src/board.js
@@ -86,7 +86,24 @@ Board.prototype.isOccupied = function (pos) {
  * Returns empty array if no pieces of the opposite color are found.
  */
 Board.prototype._positionsToFlip = function(pos, color, dir, piecesToFlip){
-  
+
+  if (!piecesToFlip){
+    piecesToFlip = [];
+  } else {
+    piecesToFlip.push(pos);
+  };
+
+  let nextPos = [pos[0] + dir[0], pos[1] + dir[1]];
+
+  if (!this.isValidPos(nextPos)) {
+    return [];
+  } else if (!this.isOccupied(nextPos)) {
+    return [];
+  } else if (this.isMine(nextPos, color)){
+    return [];
+  } else {
+    return  this._positionsToFlip(nextPos, color, dir, piecesToFlip);
+  };
 };
 
 /**
