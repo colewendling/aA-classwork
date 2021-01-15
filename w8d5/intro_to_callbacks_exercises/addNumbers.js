9a6940335e809040650be131f9125a0118af442e
JCS <joshsad2@gmail.com>
2021-01-15 14:16:21 -0800

    completed addNumbers.js

--- w8d5/intro_to_callbacks_exercises/addNumbers.js
+++ w8d5/intro_to_callbacks_exercises/addNumbers.js
@@ -1,4 +1,4 @@
-const readline = require("readline");
+const readline = require('readline');
 
 const reader = readline.createInterface({
     input: process.stdin,
@@ -6,6 +6,30 @@ const reader = readline.createInterface({
 });
 
 function addNumbers(sum, numsLeft, completionCallback) {
+    if (numsLeft > 0) {
+        reader.question("Enter Number", function (userNum) {
+            const curNum = parseInt(userNum);
+            sum += curNum;
+            console.log("The curreent sum is " + sum + ".");
+            addNumbers(sum, numsLeft - 1, completionCallback);
+        })
+    } else {
+        completionCallback(sum);
+    }
 };
 
-addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`, reader.close()));
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question('What do you think of Node.js? ', (answer) => {
+//     // TODO: Log the answer in a database
+//     console.log(`Thank you for your valuable feedback: ${answer}`);
+
+//     rl.close();
+// });
