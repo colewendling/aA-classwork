cc6cdc97f8ac9e45740de05821f2ba37b19f19c0
colewendling <colewendling@utexas.edu>
2021-01-15 16:37:21 -0600

    absurdBubbleSort javascript

--- w8d5/intro_to_callbacks_exercises/absurdBubbleSort.js
+++ w8d5/intro_to_callbacks_exercises/absurdBubbleSort.js
@@ -21,15 +21,28 @@ function askIfGreaterThan(el1, el2, callback) {
     );
 }
 
+
+
+function test()
 // Once you're done testing askIfGreaterThan with dummy arguments, write this.
 function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
     // Do an "async loop":
     // 1. If (i == arr.length - 1), call outerBubbleSortLoop, letting it
     //    know whether any swap was made.
+
     // 2. Else, use `askIfGreaterThan` to compare `arr[i]` and `arr[i +
     //    1]`. Swap if necessary. Call `innerBubbleSortLoop` again to
     //    continue the inner loop. You'll want to increment i for the
     //    next call, and possibly switch madeAnySwaps if you did swap.
+
+    if (i == (arr.length -1)) {
+        outerBubbleSortLoop(madeAnySwaps);
+        return;
+    }
+
+    
+
+
 }
 
 // Once you're done testing innerBubbleSortLoop, write outerBubbleSortLoop.
