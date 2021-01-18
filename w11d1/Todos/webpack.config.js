19415963d72c149037487e07f3173bb56e23f9a4
colewendling <colewendling@utexas.edu>
2021-01-17 19:01:57 -0600

    connect rails/react dir files // add static_pages view

--- w11d1/Todos/webpack.config.js
+++ w11d1/Todos/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/todo_redux.jsx',
+  entry: './app/assets/javascripts/frontend/todo_redux.jsx',
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: 'bundle.js'
