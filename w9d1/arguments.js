5a8321661451d16ea5f8434c63dd41e1e4292f58
colewendling <colewendling@utexas.edu>
2021-01-19 17:26:23 -0600

    curried sum function

--- w9d1/arguments.js
+++ w9d1/arguments.js
@@ -29,8 +29,10 @@ function sum2(...nums) {
 Function.prototype.myBind1 = function (context) {
   const fn = this
   const inputs = Array.from(arguments).slice(1)
+    
   return function boundFunction() {
     const inputs2 = Array.from(arguments)
+     
     return fn.apply(context, inputs.concat(inputs2))
   };
 
@@ -43,14 +45,6 @@ Function.prototype.myBind1 = function (context) {
 Function.prototype.myBind2 = function (ctx, ...bindArgs) {
   return this.apply(ctx, bindArgs)
 }
-
-
-
-
-
-
-
-
 
 
 
@@ -101,33 +95,72 @@ markov.says.myBind1(pavlov, "meow")("Markov");
 // true
 
 
+Function.prototype.curry = function (numArgs) {
+    let args = [];
 
+    return function curriedSum(arg) {
+        args.push(arg);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if (args.length === numArgs) {
+            return args.reduce((acc, el) => acc + el)    // arr.inject{|acc, el| acc + el }
+            
+        } else {
+            return curriedSum;
+        }
+    }
+    //return curriedSum;
+};
 
 
 
 function curriedSum(numArgs) {
+
 }
 
-// using spread
-Function.prototype.curry = function (numArgs) {
+
+
+// (9, 5, 6, 7, 8, 43)
+
+// Note that the curried version returns functions at each invocation until it has the full number of arguments it needs.At this point it actually invokes sumThree and returns the result.
+
+// Write a curriedSum function that takes an integer(how many numbers to sum) and returns a function that can be successively called with single arguments until it finally returns a sum.That is:
+
+// const sum = curriedSum(4); 4 is numArgs
+// sum(5)(30)(20)(1); // => 56  each of () is arg
+
+// Hint: curriedSum(numArgs) should:
+// Define an empty array, numbers.
+// Defines a function, _curriedSum that:
+
+// Closes over numArgs and numbers.
+// Takes a single number as an argument.
+// Appends this to numbers each time.
+// If numbers.length === numArgs, it sums the numbers in the array and returns the result.
+//     Else, it returns itself.
+
+
+
+
+
+function sumThree(num1, num2, num3) {
+    return num1 + num2 + num3;
 }
+
+sumThree(4, 20, 6); // == 30
+
+// you'll write `Function#curry`!
+let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+f1 = f1(4); // [Function]
+f1 = f1(20); // [Function]
+f1 = f1(6); // = 30
+
+// or more briefly:
+console.log(sumThree.curry(3)(4)(20)(6)); // == 30
+
+
+
+
+
 
 // using apply
 Function.prototype.curry1 = function (numArgs) {
