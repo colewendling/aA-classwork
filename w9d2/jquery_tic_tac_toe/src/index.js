8fcb8152d8b03b1cecde8cc73a19fa24b8218b8d
colewendling <colewendling@utexas.edu>
2021-01-20 13:33:31 -0600

    setup require files and bundle our file

--- w9d2/jquery_tic_tac_toe/src/index.js
+++ w9d2/jquery_tic_tac_toe/src/index.js
@@ -1,5 +1,7 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view');// require appropriate file
+const Game = require('../../tic_tac_toe_node_solution/game');
+
+// require appropriate file
 
   $(() => {
     // Your code here
