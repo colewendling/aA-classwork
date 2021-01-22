bb0a5702e075ddc2c5b07e49feff0a71f9b832dc
colewendling <colewendling@utexas.edu>
2021-01-22 16:11:40 -0600

    clock class - phase. 0 setup

--- w9d5/pocket_projects/src/warmup.js
+++ w9d5/pocket_projects/src/warmup.js
@@ -2,7 +2,10 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
-
+    var p = document.createElement("p");
+    var text = document.createTextNode(string)
+    p.appendChild(text);
+    htmlElement.appendChild(p);
 };
 
 htmlGenerator('Party Time.', partyHeader);
