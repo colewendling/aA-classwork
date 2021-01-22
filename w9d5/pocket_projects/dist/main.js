3234ee86fc8af4bee2c2750af172a0003111e06e
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-01-22 15:19:48 -0800

    Worked on clock and drop down

--- w9d5/pocket_projects/dist/main.js
+++ w9d5/pocket_projects/dist/main.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/clock.js":
@@ -16,7 +15,18 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _warmup__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./warmup */ \"./src/warmup.js\");\n\n\nclass Clock {\n    constructor() {\n        // 1. Create a Date object.\n        const currentTime = new Date();\n\n        // 2. Store the hour, minute, and second.\n        this.hours = currentTime.getHours();\n        this.minutes = currentTime.getMinutes();\n        this.seconds = currentTime.getSeconds();\n        (0,_warmup__WEBPACK_IMPORTED_MODULE_0__.htmlGenerator)(this.printTime(), clockElement);\n        // 3. Call printTime.\n        this.printTime();\n\n        // 4. Schedule the tick at 1 second intervals.\n        setInterval(this._tick.bind(this), 1000);\n    }\n\n    printTime() {\n        // Format the time in HH:MM:SS\n        const timeString = [this.hours, this.minutes, this.seconds].join(\":\");\n\n        // Use console.log to print it.\n        // console.log(timeString);\n        return timeString;\n    }\n\n    _tick() {\n        // 1. Increment the time by one second.\n        this._incrementSeconds();\n\n        // 2. Call printTime.\n        //this.printTime();\n        (0,_warmup__WEBPACK_IMPORTED_MODULE_0__.htmlGenerator)(clock.printTime(), clockElement);\n    }\n\n    _incrementSeconds() {\n        // 1. Increment the time by one second.\n        this.seconds += 1;\n        if (this.seconds === 60) {\n            this.seconds = 0;\n            this._incrementMinutes();\n        }\n    }\n\n    _incrementMinutes() {\n        this.minutes += 1;\n        if (this.minutes === 60) {\n            this.minutes = 0;\n            this._incrementHours();\n        }\n    }\n\n    _incrementHours() {\n        this.hours = (this.hours + 1) % 24;\n    }\n\n\n}\n\nconst clockElement = document.getElementById('clock');\nconst clock = new Clock();\n\n//htmlGenerator(clock.printTime(), clockElement);\n\n\n//# sourceURL=webpack:///./src/clock.js?");
+
+/***/ }),
+
+/***/ "./src/drop_down.js":
+/*!**************************!*\
+  !*** ./src/drop_down.js ***!
+  \**************************/
+/***/ (() => {
+
+eval("\nconst dogs = {\n  \"Corgi\": \"https://www.akc.org/dog-breeds/cardigan-welsh-corgi/\",\n  \"Australian Shepherd\": \"https://www.akc.org/dog-breeds/australian-shepherd/\",\n  \"Affenpinscher\": \"https://www.akc.org/dog-breeds/affenpinscher/\",\n  \"American Staffordshire Terrier\": \"https://www.akc.org/dog-breeds/american-staffordshire-terrier/\",\n  \"Tosa\": \"https://www.akc.org/dog-breeds/tosa/\",\n  \"Labrador Retriever\": \"https://www.akc.org/dog-breeds/labrador-retriever/\",\n  \"French Bulldog\": \"https://www.akc.org/dog-breeds/french-bulldog/\" \n};\nconst dogLinkCreator = (dogs) =>{\n  const dogLinks = [];\n  const dogNames = Object.keys(dogs);\n  dogNames.forEach(dog => {\n    const outerLi = document.createElement(\"li\");\n    const dogLink = document.createElement(\"a\");\n    dogLink.innerHTML = dog;\n    dogLink.href = dogs[dog];\n    outerLi.classList = \"dog-link\";\n    outerLi.appendChild(dogLink);\n    dogLinks.push(outerLi);\n  });\n  return dogLinks;\n};\nconst attachDogLinks = () =>{\n  const dogLinks = dogLinkCreator(dogs);\n  const dropDownList = document.querySelector(\".drop-down-dog-list\");\n  dogLinks.forEach(link => dropDownList.appendChild(link));\n};\nattachDogLinks();\nconst handleLeave = ()=>{\n  const dogLinks = document.querySelectorAll(\".dog-link\");\n  dogLinks.forEach(child => child.classList.remove(\"open\"));\n};\nconst handleEnter = ()=>{\n  const dogLinks = document.querySelectorAll(\".dog-link\");\n  dogLinks.forEach(child => child.classList.add(\"open\"));\n};\nconst nav = document.querySelector(\".drop-down-dog-nav\");\nnav.addEventListener(\"mouseenter\", handleEnter);\nnav.addEventListener(\"mouseleave\", handleLeave);\n\n\n\n\n//# sourceURL=webpack:///./src/drop_down.js?");
 
 /***/ }),
 
@@ -26,7 +36,8 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _war
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _warmup__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./warmup */ \"./src/warmup.js\");\n/* harmony import */ var _clock__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./clock */ \"./src/clock.js\");\n\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _warmup__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./warmup */ \"./src/warmup.js\");\n/* harmony import */ var _clock__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./clock */ \"./src/clock.js\");\n/* harmony import */ var _drop_down__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./drop_down */ \"./src/drop_down.js\");\n/* harmony import */ var _drop_down__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_drop_down__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 
@@ -36,6 +47,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _war
   \***********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"htmlGenerator\": () => /* binding */ htmlGenerator\n/* harmony export */ });\n\nconst partyHeader = document.getElementById('party');\n\nconst htmlGenerator = (string, htmlElement) => {\n\n    if (htmlElement.children) {\n        Array.from(htmlElement.children).forEach((child) => htmlElement.removeChild(child));\n    }\n\n\n    var p = document.createElement(\"p\");\n    var text = document.createTextNode(string)\n    p.appendChild(text);\n    htmlElement.appendChild(p);\n};\n\nhtmlGenerator('Party Time.', partyHeader);\n\n//# sourceURL=webpack:///./src/warmup.js?");
 
 /***/ })
@@ -66,6 +78,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => module['default'] :
+/******/ 				() => module;
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
--- w9d5/pocket_projects/src/drop_down.js
+++ w9d5/pocket_projects/src/drop_down.js
@@ -8,4 +8,36 @@ const dogs = {
   "Labrador Retriever": "https://www.akc.org/dog-breeds/labrador-retriever/",
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
+const dogLinkCreator = (dogs) =>{
+  const dogLinks = [];
+  const dogNames = Object.keys(dogs);
+  dogNames.forEach(dog => {
+    const outerLi = document.createElement("li");
+    const dogLink = document.createElement("a");
+    dogLink.innerHTML = dog;
+    dogLink.href = dogs[dog];
+    outerLi.classList = "dog-link";
+    outerLi.appendChild(dogLink);
+    dogLinks.push(outerLi);
+  });
+  return dogLinks;
+};
+const attachDogLinks = () =>{
+  const dogLinks = dogLinkCreator(dogs);
+  const dropDownList = document.querySelector(".drop-down-dog-list");
+  dogLinks.forEach(link => dropDownList.appendChild(link));
+};
+attachDogLinks();
+const handleLeave = ()=>{
+  const dogLinks = document.querySelectorAll(".dog-link");
+  dogLinks.forEach(child => child.classList.remove("open"));
+};
+const handleEnter = ()=>{
+  const dogLinks = document.querySelectorAll(".dog-link");
+  dogLinks.forEach(child => child.classList.add("open"));
+};
+const nav = document.querySelector(".drop-down-dog-nav");
+nav.addEventListener("mouseenter", handleEnter);
+nav.addEventListener("mouseleave", handleLeave);
+
 
