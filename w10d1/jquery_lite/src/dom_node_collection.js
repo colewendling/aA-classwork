183e6897989fb4e833f9d522b0ebafb22806824e
Janette <tinoconjanette@gmail.com>
2021-01-25 15:27:50 -0800

    Finish Append in JQUERY Lite

--- w10d1/jquery_lite/src/dom_node_collection.js
+++ w10d1/jquery_lite/src/dom_node_collection.js
@@ -5,5 +5,32 @@ class DOMNodeCollection {
 };
 
 
-module.exports = DOMNodeCollection;
+DOMNodeCollection.protoype.html = function(string){
+    if(string){//if it receives argument puts string
+        return this.innerHTML = string
+    }else {
+        return this.collection[0].innerHTML
+    }
+}
 
+
+DOMNodeCollection.protoype.empty = function(){
+    this.collection.map(function(node){
+        node = "";
+    })
+}
+
+DOMNodeCollection.protoype.append = function(arg){
+    if(arg instanceof jQuery){
+        this.collection.forEach((node) => {
+            //append arg.outerhtml to node.innerhtml
+
+            let inner = document.getElementsByTagName(node[0])[0].innerHTML;
+            inner += document.getElementsByTagName(arg[0])[0].outerHTML;
+        })
+    }else if()
+}
+
+
+
+module.exports = DOMNodeCollection;
