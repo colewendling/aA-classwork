1ae8a13f53f22d3ea05dea8e4c9275f4ee402bd5
colewendling <colewendling@utexas.edu>
2021-01-26 17:33:08 -0600

    render clock tick correctly on browser

--- w10d2/widgets/frontend/clock.jsx
+++ w10d2/widgets/frontend/clock.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 class Clock extends React.Component{
   constructor(props){
+    super(props); //need to put this if you are using this
+
+
     this.state = { time: new Date() };
     this.tick = this.tick.bind(this);
   }
@@ -16,8 +19,11 @@ class Clock extends React.Component{
 
 
   render(){
-    return (
+    return (      // toString(this.state.time)
+      <>
       <h1>Clock</h1>
+        <h2>{this.state.time.toString()}</h2>
+      </>
     )
   }
 }
