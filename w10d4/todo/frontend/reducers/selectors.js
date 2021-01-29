82ec046cc6b93f58fc3f38b5541bf8463699a7ea
colewendling <colewendling@utexas.edu>
2021-01-29 17:12:23 -0600

    tested selector and fix selectors.js:6

--- w10d4/todo/frontend/reducers/selectors.js
+++ w10d4/todo/frontend/reducers/selectors.js
@@ -1,7 +1,10 @@
-export const allTodos = ({ todos }) => {
+export const allTodos = (state) => { //state: state
     return (
-        Object.keys(todos).map( id =>
-            todos[id]    
-        );
-    );
+        // Object.keys(todos).map( id =>
+        //     todos[id]    
+        // );
+        Object.values(state.todos)
+            //todos[id]
+        
+    )
 };
--- w10d4/todo/frontend/todo_redux.jsx
+++ w10d4/todo/frontend/todo_redux.jsx
@@ -4,6 +4,31 @@ import { receiveTodo, removeTodo, receiveTodos } from './actions/todo_actions';
 import { receiveStep, removeStep, receiveSteps } from './actions/step_actions';//change to steps
 import configureStore from './store/store';
 import Root from './components/root';
+import {allTodos} from './reducers/selectors';
+
+
+
+
+
+const todos = { 
+    1:  { id: 1, title: 'Learn Redux', body: 'It is fundamental', done: false },
+    2:  { id: 2, title: 'Pass React', body: 'do this', done: true },
+    3:  { id: 3, title: 'Finish Project', body: 'W10D5', done: false }
+};
+
+const steps = {
+    1:  { id: 1, title: 'Dispatch actions', done: false, todo_id: 1 }
+};
+
+const preloadedState = {
+    todos, //todos: todos
+    steps, //steps: steps
+};
+
+
+// const newTodos = [{ id: 1, title: 'Learn Redux', body: 'It is fundamental', done: false }, { id: 2, title: 'Learn Redux', body: 'It is fundamental', done: false }, { id: 3, title: 'Learn Redux', body: 'It is fundamental', done: false }];
+
+// const newSteps = [{ id: 1, title: 'Dispatch actions', done: false, todo_id: 1 }, { id: 1, title: 'Dispatch actions', done: false, todo_id: 1 }, { id: 1, title: 'Dispatch actions', done: false, todo_id: 2 }];
 
 document.addEventListener("DOMContentLoaded", () => {
     // window.store = configureStore();
@@ -13,13 +38,14 @@ document.addEventListener("DOMContentLoaded", () => {
     window.receiveStep = receiveStep;
     window.receiveSteps = receiveSteps;
     window.removeStep = removeStep;
+    window.allTodos = allTodos;
 
     // const preloadedState = localStorage.state ?
     // JSON.parse(localStorage.state) : {};
 
     // const store = configureStore(preloadedState);
 
-    const store = configureStore();
+    const store = configureStore(preloadedState);
     window.store = store;
 
     const root = document.getElementById("root");
