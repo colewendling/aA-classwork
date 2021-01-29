83fb3c542165ec1ea3b987f9f257ba29cd950ab6
colewendling <colewendling@utexas.edu>
2021-01-29 17:41:43 -0600

    connect TodoListContainer to App react component

--- w10d4/todo/frontend/components/app.jsx
+++ w10d4/todo/frontend/components/app.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import TodoListContainer from './todos/todo_list_container';
 
 const App = () => {
   return (
     <div>
       <h1>react components go here</h1>
+      <TodoListContainer />
     </div>
   )
 };
