d98d81b068a64312bfc6ef8d04c5938294d0fb3d
appkenny <kzeng.95@gmail.com>
2021-01-28 17:01:17 -0800

    added steps and debugged

---
+++ w10d4/todo/frontend/reducers/steps_reducers.js
@@ -0,0 +1,31 @@
+import { RECEIVE_STEP, RECEIVE_STEPS, REMOVE_STEP } from '../actions/step_actions';
+
+const stepsReducer = (state = {}, action) => {
+  let nextState;
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_STEPS:
+      action.steps.forEach( step => {
+        nextState[step.id] = step;
+      });
+      return nextState;
+
+    case RECEIVE_STEP:
+      
+      // const newstep = { [action.step.id]: action.step };
+      let stepID = action.step.id; 
+      const newStep = { [stepID]: action.step }; 
+      return Object.assign({}, state, newStep,);
+
+    case REMOVE_STEP:
+      nextState = Object.assign({}, state);
+      delete nextState[action.step.id];
+      return nextState;
+
+    default:
+      return state;
+  }
+};
+
+export default stepsReducer;
--- w10d4/todo/frontend/todo_redux.jsx
+++ w10d4/todo/frontend/todo_redux.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { receiveTodo } from './actions/todo_actions';
+import { receiveTodo, removeTodo, receiveTodos } from './actions/todo_actions';
+import { receiveStep, removeStep, receiveSteps } from './actions/step_actions';//change to steps
 
 
 import configureStore from './store/store'
@@ -10,4 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
     // ReactDOM.render(<h1>Todos App</h1>, root); 
     window.store = configureStore();
     window.receiveTodo = receiveTodo;
+    window.receiveTodos = receiveTodos;
+    window.removeTodo = removeTodo;
+    window.receiveStep = receiveStep;
+    window.receiveSteps = receiveSteps;
+    window.removeStep = removeStep;
 });
