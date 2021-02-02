be24a4fb97048d4ba0dbaee433709e5975efe419
colewendling <colewendling@utexas.edu>
2021-02-02 17:56:47 -0600

    api_util.js fetchAllPokemon AJAX request

---
+++ w11d2/pokedex/frontend/util/api_util.js
@@ -0,0 +1,14 @@
+//Inside this file, we'll define functions that make ajax requests, fetching information from our rails api.
+
+import { $CombinedState } from "redux"
+
+// Export a function called fetchAllPokemon that returns a promise.
+// The function should make an AJAX request that will make a http request to our PokemonController#index endpoint.
+// Run rails routes to determine the appropriate url for this request.
+
+export const fetchAllPokemon = () => {
+    return $.ajax({
+        method: "GET",
+        url: "/api/pokemon"
+    })
+}
